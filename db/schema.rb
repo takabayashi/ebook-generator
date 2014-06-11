ec2d7400281742d227f1dca97f6d84fcf8d62b01
Daniel Martins Takabayashi <takabayashi@knowlodge.com.br>
2014-06-11 14:04:33 -0300

    - inclusão do aws para upload - inclusão do pace para loading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611111519) do
+ActiveRecord::Schema.define(version: 20140611164307) do
 
   create_table "book_templates", force: true do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140611111519) do
     t.integer  "default_width"
     t.integer  "default_height"
     t.string   "temp_link"
+    t.text     "configurations"
   end
 
   create_table "publishers", force: true do |t|
